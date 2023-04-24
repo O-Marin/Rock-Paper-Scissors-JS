11c141cfece7d8dd60c44a0c64b40617ff00ebac
Omei <omarancb@gmail.com>
2023-04-24 14:19:48 -0400

    feat: single run of play RPS complet, returns winner, or draw if draw

--- jsRPS/RPS.js
+++ jsRPS/RPS.js
@@ -1,5 +1,32 @@
+choices = ["rock","paper","scissors"]
+
 function getComputerChoice(){
-    choices = ["rock","paper","scissors"]
+    
     var randomAnswer = choices[Math.floor(Math.random() * choices.length)];
     return randomAnswer
 }
+
+function playRPS(playerSelection, computerSelection){
+    
+    beats = {'paper':'rock', 'rock':'scissors', "scissors":'paper'}
+    if (playerSelection == beats[computerSelection])
+        return "Computer wins, " + computerSelection  + " beats " + playerSelection
+    else if(computerSelection == beats[playerSelection])
+        return "Player wins, " + playerSelection  + " beats " + computerSelection
+    else if(computerSelection == playerSelection)
+        return "Draw"
+    
+}
+
+let pick = prompt("Please select an option");
+let playerSelection = pick.toLowerCase();
+
+while (playerSelection != "rock" && playerSelection != "scissors" && playerSelection != "paper")
+    pick = prompt("please enter a valid option")
+
+playerSelection = pick.toLowerCase();
+computerSelection = getComputerChoice();
+
+
+
+console.log(playRPS(playerSelection, computerSelection));
